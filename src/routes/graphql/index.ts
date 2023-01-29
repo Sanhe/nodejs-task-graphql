30f8789f8d332e12afd8e3f4f934bad625db578d
Alexey Shevtsov <a.shevtsov@itransition.com>
2023-01-30 00:13:15 +0100

    feat: implement depth-limit validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,11 +1,15 @@
 import { FastifyInstance } from 'fastify';
 import { graphql } from 'graphql/graphql';
 import { GraphQLSchema } from 'graphql/type';
+import { parse } from 'graphql/language';
+import { validate } from 'graphql/validation';
+import * as depthLimit from 'graphql-depth-limit';
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
 import { getQuery } from './querySchema';
 import { GraphQLRequestType } from './graphQLRequestType';
 import { getMutation } from './mutationSchema';
+import { DEPTH_LIMIT } from './configs';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify: FastifyInstance): Promise<void> => {
   fastify.post(
@@ -22,6 +26,20 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify: FastifyInstance
         query: await getQuery(fastify),
         mutation: await getMutation(fastify),
       });
+
+      const parsedQuery = parse(String(query));
+      const depthLimitRule = depthLimit(DEPTH_LIMIT);
+
+      const errors = validate(schema, parsedQuery, [depthLimitRule]);
+
+      if (errors.length > 0) {
+        const errorResponse = {
+          errors,
+          data: null,
+        };
+
+        return errorResponse;
+      }
 
       const response = await graphql({
         schema,
